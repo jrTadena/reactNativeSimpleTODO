ad3db803b205fc5c3baf9f62507c8fe932db3773
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-11-17 23:38:15 +0800

    Add eslint configure linter

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable quotes */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'eslint:recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'double'],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
+};
