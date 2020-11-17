8558ae1b257547abfa49120df431d674b7025b0b
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-11-18 07:19:28 +0800

    Change App file

--- App.js
+++ App.js
@@ -8,17 +8,47 @@ import {
   Switch,
 } from 'react-native';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: '',
+          completed: false,
+        },
+      ];
+    }
+  }
+};
+
+const DataContext = createContext();
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <DataContext.Provider value={dispatch}>
+      <View style={styles.container}>
+        <Text>Simple TODO App</Text>
+        <Button
+          onPress={() => {
+            dispatch({ type: 'add' });
+          }}
+          title='Add TODO'
+        />
+      </View>
+      <View style={styles.container}>
+        {state.map((item) => (
+          <Text>{item.id}</Text>
+        ))}
+      </View>
+    </DataContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
