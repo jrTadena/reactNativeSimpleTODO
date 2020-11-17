5af332f6a06acc58844c5de7d55d3d36deebf20e
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-11-18 06:39:44 +0800

    Modified App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer, useContext, createContext } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Switch,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
     </View>
   );
 }
