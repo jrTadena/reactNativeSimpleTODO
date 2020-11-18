9e36b09c3d643b4dd42e2e67167ec3da475818a8
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-11-18 13:32:32 +0800

    Adding functionality in switch

--- App.js
+++ App.js
@@ -23,6 +23,20 @@ const reducer = (state, action) => {
     case 'delete': {
       return state.filter((item) => item.id !== action.payload);
     }
+    case 'completed': {
+      return state.map((item) => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      });
+    }
+    default: {
+      return state;
+    }
   }
 };
 
@@ -71,6 +85,10 @@ const ItemTodos = ({ id, text, completed }) => {
           trackColor={{ false: 'lightgray', true: 'magenta' }}
           thumbColor={completed ? 'magenta' : 'lightgray'}
           ios_backgroundColor='lightgray'
+          onValueChange={() =>
+            value.dispatch({ type: 'completed', payload: id })
+          }
+          value={completed}
         />
       </View>
       <View style={styles.textInputContainer}>
